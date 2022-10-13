309be1524ba8602d8d42c7efed40a2d1786097c4
AbdulRahman MAFE <Kemimafe11@gmail.com>
2022-10-13 09:34:52 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description - To display using printf
+ */
 int main(void) {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
