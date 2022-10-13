2f73ea06c5507ab39d4fba0ee5852f79892cb72f
AbdulRahman MAFE <Kemimafe11@gmail.com>
2022-10-13 13:06:49 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+
+
+
+/**
+ *
+ * main - Entry point
+ *
+ *
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
 int main(void)
+
 {
 
+		puts("\"Programming is like building a multilingual puzzle");
 
-    puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
-
+			return (0);
 
 }
