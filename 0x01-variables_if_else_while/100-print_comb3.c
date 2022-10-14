0309b1bfa4b51d813ec4ea3e7b68ebf82fcbadcc
AbdulRahman MAFE <Kemimafe11@gmail.com>
2022-10-14 14:47:11 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,19 +3,19 @@
  * main - Entry point
  *
  *
- * Return : Always 0 (Success/valid)
+ * Return: Always 0 (Success/valid)
  */
 int main(void)
 {
 int number_left;
 int number_right;
-for (number_left = 48; number_right <= 78; number_left++)
+for (number_left = 48; number_right <= 58; number_left++)
 {
-for (number_right = number_left + 1; number_right <= 78; number_right++)
+for (number_right = number_left + 1; number_right <= 58; number_right++)
 {
 putchar(number_left);
 putchar(number_right);
-if ((number_left == 56) && (number_right == 78))
+if ((number_left == 56) && (number_right == 57))
 {
 break;
 }
