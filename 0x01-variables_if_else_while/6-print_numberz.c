87bd69abbd8aa8fc2d56e293456796d1b366d51e
AbdulRahman MAFE <Kemimafe11@gmail.com>
2022-10-14 14:04:36 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+for (i = 49; i <= 57; i++)
+{
+putchar (i);
+}
+putchar('\n');
+return (0);
+}
