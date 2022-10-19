a6f0d47d09c93f93a67a147cfae4f84a60bf3479
AbdulRahman MAFE <Kemimafe11@gmail.com>
2022-10-18 17:04:14 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,13 @@
  * print_alphabet_x10 - prints alphabets 10
  *
  * Description: prints 10 times
+ *
  * Return: Always(0).
+ *
  */
 
-void print_alphabet(void)
+
+void print_alphabet_x10(void)
 {
 int alphabet;
 int increase;
