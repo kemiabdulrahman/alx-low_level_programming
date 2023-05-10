4ca5067237ec31b34e2e9164aa9931a9c040c0ea
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-05-10 22:42:19 +0100

    AbdulRahman MAFE

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,8 +11,8 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low, high;
-	double f;
+	size_t position, low, high;
+	double probe;
 
 	if (array == NULL)
 		return (-1);
@@ -22,27 +22,27 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (size)
 	{
-		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
-		printf("Value checked array[%d]", (int)pos);
+		probe = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		position = (size_t)(low + probe);
+		printf("Value checked array[%d]", (int)position);
 
-		if (pos >= size)
+		if (position >= size)
 		{
 			printf(" is out of range\n");
 			break;
 		}
 		else
 		{
-			printf(" = [%d]\n", array[pos]);
+			printf(" = [%d]\n", array[position]);
 		}
 
-		if (array[pos] == value)
-			return ((int)pos);
+		if (array[position] == value)
+			return ((int)position);
 
-		if (array[pos] < value)
-			low = pos + 1;
+		if (array[position] < value)
+			low = position + 1;
 		else
-			high = pos - 1;
+			high = position - 1;
 
 		if (low == high)
 			break;
--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -15,7 +15,7 @@
   */
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
-	size_t i;
+	size_t u;
 
 	if (array == NULL)
 		return (-1);
@@ -23,17 +23,17 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 	while (right >= left)
 	{
 		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+		for (u = left; u < right; u++)
+			printf("%d, ", array[u]);
+		printf("%d\n", array[u]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
+		u = left + (right - left) / 2;
+		if (array[u] == value)
+			return (u);
+		if (array[u] > value)
+			right = u - 1;
 		else
-			left = i + 1;
+			left = u + 1;
 	}
 
 	return (-1);
